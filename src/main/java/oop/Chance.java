46e6055de99b595d02b9e0cf6f36ed4e573e44cf
Irene Torres <irene.t@thoughtworks.com>
2022-03-10 11:03:43 +0100

    space to test

--- src/main/java/oop/Chance.java
+++ src/main/java/oop/Chance.java
@@ -4,6 +4,7 @@ public class Chance {
 
   static public double calculateChanceOfTwoEvents(double eventA, double eventB) {
     return eventA * eventB;
+    
   }
 
   static public double calculateChanceOfOccurrenceOfOneEventOrTheOther(double eventA, double eventB) {
